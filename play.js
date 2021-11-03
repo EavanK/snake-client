6d27762627755045a5804e0548f7efb6e48dfd89
Hyunsu Kim <ksoksl0711@gmail.com>
2021-11-03 22:00:07 +0000

    stdin setup

--- play.js
+++ play.js
@@ -1,1 +1,22 @@
 const connect = require('./client');
+
+// setup interface to handle user input from stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = (key) => {
+    //we can exit by CTRL + C
+    if (key === '\u0003') {
+      process.exit();
+    }
+  };
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
